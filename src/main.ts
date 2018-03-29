1a413821e5be11c8fbc28d3a742c3fa9b4a64195
Matteo Piazza <matteo.piazza@exmachina.ch>
2018-03-29 14:52:24 +0200

    config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, ip);
+  await app.listen(port);
 }
 bootstrap();
