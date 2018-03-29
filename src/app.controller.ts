d7eef33fb46393374df139a73512a766ddf6e8d8
Matteo Piazza <matteo.piazza@exmachina.ch>
2018-03-29 15:33:55 +0200

    config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,9 +6,9 @@ type User = {
 };
 
 let users: User[] = [
-  {id: 1, name: 'Jack'},
-  {id: 2, name: 'John'},
-  {id: 3, name: 'Lara'}
+  {id: 1, name: 'Jack x1'},
+  {id: 2, name: 'John x2'},
+  {id: 3, name: 'Lara x3'}
 ];
 
 @Controller('api/users')
